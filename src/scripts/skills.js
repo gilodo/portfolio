740097dd786f47179801b97d49c2c196d536cadb
Михаил <mgilodo@gmail.com>
2019-03-30 15:12:19 +0200

    Сделал скилы на vue

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,50 @@
-console.log('this is skills module');
+import Vue from 'vue';
+
+const skill = {
+    template: "#skill",
+    props: {
+        skillNameComp: String,
+        skillPercentComp: Number
+    },
+    methods: {
+        drawColoredCircle() {
+            const circle = this.$refs['color-circle'];
+            const dashArray = parseInt(
+            getComputedStyle(circle).getPropertyValue('stroke-dasharray')
+            );
+            const percent = (dashArray / 100) * (100 - this.skillPercentComp);
+
+            circle.style.strokeDashoffset = percent;
+        }
+    },
+    mounted() {
+        this.drawColoredCircle();
+    }
+}
+
+const skillsRow = {
+    template: "#skills-row",
+    components: {
+        skill
+    },
+    props: {
+        skillRowObject: Object
+    }
+}
+
+new Vue ({
+    el: "#skills-component",
+    template: "#skills-list",
+    components: {
+        skillsRow
+    },
+    data() {
+        return {
+            skillsData: {}
+        }
+    },
+    created() {
+        const data = require("../data/skills.json");
+        this.skillsData = data;
+    }
+})
---
+++ src/scripts/works.js
@@ -0,0 +1,119 @@
+import Vue from "vue";
+
+const display = {
+    template: "#slider-display",
+    components: {
+        btns, thumbs
+    },
+    props: {
+        works: Array,
+        currentWork: Object,
+        currentIndex: Number
+    },
+    computed: {
+        reversedWorks() {
+            const works = [...this.works];
+            return works.reverse();
+        }
+    }
+};
+
+const thumbs = {
+    template: "#slider-thumbs",
+    props: {
+        works: Array,
+        currentWork: Object
+    }
+};
+
+const btns = {
+    template: "#slider-btns",
+    methods: {
+        slide(direction) {
+          switch (direction) {
+            case "next":
+              break;
+            case "prev":
+              break;
+          }
+        }
+      }
+};
+
+const tags = {
+    template: "#slider-tags",
+    props: {
+        tagsArray: Array
+    }
+};
+
+const info = {
+    template: "#slider-info",
+    components: {
+        tags
+    },
+    props: {
+        currentWork: Object
+    },
+    computed: {
+        tagsArray() {
+            return this.currentWork.skills.split(',')
+        }
+    }
+};
+
+new Vue ({
+    template: "#slider-container",   // Откуда взять
+    el: "#slider-components",         // Куда примонтировать    
+    components: {
+        display,
+        info
+    },
+    data() {
+        return {
+            works: [],
+            currentIndex: 0
+        };
+    },
+    computed: {
+        currentWork() {
+            return this.works[this.currentIndex];
+        }
+    },
+    watch: {
+        currentIndex(value) {
+            this.makeInfiniteLoopForCurIndex(value);
+        }
+    },
+    methods: {
+        makeInfiniteLoopForCurIndex(value) {
+            const worksAmount = this.works.length - 1;
+            if (value > worksAmount) this.currentIndex = 0;
+            if (value < 0) this.currentIndex = worksAmount;
+        },
+        makeArrWithRequiredImages(data) {
+            return data.map(item => {
+                const requiredPic = require(`../images/content/${item.photo}`);
+                item.photo = requiredPic;
+
+                return item;
+            })
+        },
+        handleSlide(direction) {
+            switch (direction) {
+                case 'next':
+                    this.currentIndex++;
+                    break;
+                case 'prev':
+                    this.currentIndex--;
+                    break;
+            }
+        }
+    },
+    created() {
+        const data = require('../data/works.json');
+        this.works = this.makeArrWithRequiredImages(data);
+
+        this.works = this.works;
+    }
+})
