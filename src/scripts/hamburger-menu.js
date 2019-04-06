a0ea709deee39ec24f8aeb83f405aa4b4d1609ed
Михаил <mgilodo@gmail.com>
2019-04-06 22:40:47 +0300

    добавлен код vue-flickity (не работает)

--- src/scripts/hamburger-menu.js
+++ src/scripts/hamburger-menu.js
@@ -1,9 +1,9 @@
 // МЕНЮ-ГАМБУРГЕР
 const burger = document.querySelector(".hamburger-menu");
-const navBlock = document.querySelector(".nav");
+const navBlock = document.querySelector(".header__menu");
 
 burger.addEventListener("click", function() {
   burger.classList.toggle("active");
-  navBlock.classList.toggle("active");
+  navBlock.classList.toggle("header__menu_mobile");
   document.body.classList.toggle("hidden");
 });
